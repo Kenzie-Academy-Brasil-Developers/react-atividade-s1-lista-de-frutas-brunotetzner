a52e8f00fbd75aa2288401a850ad4df999c65f4d
Bruno Tetzner <brunoltetzner@gmail.com>
2021-10-28 14:18:24 -0300

    refatorando e otimizando metodo map

--- src/App.js
+++ src/App.js
@@ -7,29 +7,29 @@ function App() {
     { name: "cherry", color: "red", price: 3 },
     { name: "strawberry", color: "red", price: 4 },
   ]);
-  const MapObj = fruits.map((item) => {
-    return item.name;
-  });
+
   function filterRedFruits() {
     const FilterObj = fruits.filter((item) => {
       return item.color === "red";
     });
     setFruits(FilterObj);
   }
-  const Totalprice = fruits.reduce((acumulador, vAtual) => {
-    return acumulador + vAtual.price;
-  }, 0);
   return (
     <div className="App">
       <header className="App-header"></header>
       <div id="price">
-        <p>R$: {Totalprice}</p>
+        <p>
+          R$:
+          {fruits.reduce((acumulador, vAtual) => {
+            return acumulador + vAtual.price;
+          }, 0)}
+        </p>
       </div>
-      <div id="fruits-name">
-        <li>{MapObj[0]}</li>
-        <li>{MapObj[1]}</li>
-        <li>{MapObj[2]}</li>
-      </div>
+      <ul id="fruits-name">
+        {fruits.map((item) => {
+          return <li>{item.name}</li>;
+        })}
+      </ul>
 
       <button onClick={filterRedFruits}>Apenas frutas vermelhas</button>
     </div>
